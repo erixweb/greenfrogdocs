f2668a61e872a660d923307e43ea8d50e8898ca0
erixweb <erikmorenodev@gmail.com>
2023-09-11 10:54:54 +0200

    Add tailwind (not finished)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import deno from "@astrojs/deno";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: deno(),
+  integrations: [tailwind()]
 });
